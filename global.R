192295a0e0fcc0864181cf6f59809aac477125c7
jordancampbell23 <jordan.campbell@reason.org>
2022-07-06 10:56:17 -0700

    Initial commit

---
+++ global.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(shinyjs)
+# library(shinyBS)
+library(shinycssloaders)
+library(shinyWidgets)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(echarts4r)
+library(reactable)
+library(zoo)
+library(openxlsx)
+library(readxl)
+library(prompter)
+library(stringr)
+
+#---------------------------FUNCTIONS & SETTINGS--------------------------------
+source("backend/ui.R")
+source("backend/benefit_model.R")
+source("backend/funding_model.R")
+options(spinner.color = "#ff6633", spinner.size = 1, spinner.type = 8)
+
+
+#--------------------------------MODULES----------------------------------------
+# source("modules/mod_login.R")
+source("modules/mod_historical.R")
+source("modules/mod_deterministic.R")
+source("modules/mod_benefits.R")
